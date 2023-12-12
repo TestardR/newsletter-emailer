619fa961994d637b0b65af65b30baf7afda1ffa3
romain testard <romain.rtestard@gmail.com>
2023-12-12 11:35:06 +0100

    refactor(http-server): leverage tcplistener to efficiently bind ports in IT

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,19 +1,30 @@
+use std::net::TcpListener;
+
 #[tokio::test]
 async fn health_check_works() {
-    spawn_app();
+    // Arrange
+    let address= spawn_app();
     let client = reqwest::Client::new();
 
+    // Act
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
 
+    // Assert
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() {
-    let server = newsletterEmailer::run().expect("Failed to bind address");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0")
+        .expect("Failed to bind random port");
+// We retrieve the port assigned to us by the OS
+    let port = listener.local_addr().unwrap().port();
+    let server = newsletterEmailer::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
 }
