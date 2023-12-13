dbeebc295b0bbebcf1cf480078323647d9bd2fbf
romain testard <romain.rtestard@gmail.com>
2023-12-13 15:17:46 +0100

    feat(subscription): add IT add database configuration boilterplate for future ITs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
+use newsletter_emailer::configuration::get_configuration;
+use newsletter_emailer::startup::run;
+use sqlx::PgPool;
 use std::net::TcpListener;
-use newsletterEmailer::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let address = TcpListener::bind("127.0.0.1:8000")?;
-    run(address)?.await
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection_pool)?.await
 }
