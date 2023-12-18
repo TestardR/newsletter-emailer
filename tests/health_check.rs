2728397989c4844cf6ff7bb3992d7b232c83de07
romain testard <romain.rtestard@gmail.com>
2023-12-18 17:02:31 +0100

    feat(structure-logging): replace tracing code with macros and hide sensitive variables

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -2,6 +2,7 @@ use newsletter_emailer::configuration::{get_configuration, DatabaseSettings};
 use newsletter_emailer::startup::run;
 use newsletter_emailer::telemetry::{get_subscriber, init_subscriber};
 use once_cell::sync::Lazy;
+use secrecy::ExposeSecret;
 use sqlx::Executor;
 use sqlx::{Connection, PgConnection, PgPool};
 use std::net::TcpListener;
@@ -49,16 +50,17 @@ async fn spawn_app() -> TestApp {
 
 pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
     // Create database
-    let mut connection = PgConnection::connect(&config.connection_string_without_db())
-        .await
-        .expect("Failed to connect to Postgres");
+    let mut connection =
+        PgConnection::connect(&config.connection_string_without_db().expose_secret())
+            .await
+            .expect("Failed to connect to Postgres");
     connection
         .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
         .await
         .expect("Failed to create database.");
 
     // Migrate database
-    let connection_pool = PgPool::connect(&config.connection_string())
+    let connection_pool = PgPool::connect(&config.connection_string().expose_secret())
         .await
         .expect("Failed to connect to Postgres.");
     sqlx::migrate!("./migrations")
